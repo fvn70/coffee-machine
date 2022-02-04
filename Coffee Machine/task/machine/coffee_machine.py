4694f5e8dd82c1091232c0dd2402995e90bfa5fb
fvn70 <fvn_reg@mail.ru>
2022-02-04 18:51:28 +0700

    Coffee Machine. Stage 4. Buy, fill, take

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,16 +1,46 @@
-from math import floor
+cm = {'water': 400, 'milk': 540, 'beans': 120, 'cups': 9, 'sum': 550}
+kinds = {1: {'water': 250, 'milk': 0, 'beans': 16, 'cost': 4},
+         2: {'water': 350, 'milk': 75, 'beans': 20, 'cost': 7},
+         3: {'water': 200, 'milk': 100, 'beans': 12, 'cost': 6}}
 
-cup = {'water': 200, 'milk': 50, 'beans': 15}
-water = int(input("Write how many ml of water the coffee machine has:\n"))
-milk = int(input("Write how many ml of milk the coffee machine has:\n"))
-beans = int(input("Write how many grams of coffee beans the coffee machine has:\n"))
-num = int(input("Write how many cups of coffee you will need:\n"))
 
-cups = floor(min(water / cup['water'], milk / cup['milk'], beans / cup['beans']))
+def state():
+    print("\nThe coffee machine has:")
+    print(f"{cm['water']} of water")
+    print(f"{cm['milk']} of milk")
+    print(f"{cm['beans']} of coffee beans")
+    print(f"{cm['cups']} of disposable cups")
+    print(f"{cm['sum']} of money")
 
-if cups < num:
-    print(f"No, I can make only {cups} cups of coffee")
-elif cups > num:
-    print(f"Yes, I can make that amount of coffee (and even {cups - num} more than that)")
-else:
-    print("Yes, I can make that amount of coffee")
+
+def buy():
+    print("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    kind = int(input())
+    cm['water'] -= kinds[kind]['water']
+    cm['milk'] -= kinds[kind]['milk']
+    cm['beans'] -= kinds[kind]['beans']
+    cm['sum'] += kinds[kind]['cost']
+    cm['cups'] -= 1
+
+
+def fill():
+    cm['water'] += int(input("Write how many ml of water you want to add:\n"))
+    cm['milk'] += int(input("Write how many ml of milk you want to add:\n"))
+    cm['beans'] += int(input("Write how many grams of coffee beans you want to add:\n"))
+    cm['cups'] += int(input("Write how many disposable coffee cups you want to add:\n"))
+
+
+def take():
+    print(f"I gave you ${cm['sum']}")
+    cm['sum'] = 0
+
+
+state()
+cmd = input("\nWrite action (buy, fill, take):\n")
+if cmd == 'buy':
+    buy()
+elif cmd == 'fill':
+    fill()
+elif cmd == 'take':
+    take()
+state()
