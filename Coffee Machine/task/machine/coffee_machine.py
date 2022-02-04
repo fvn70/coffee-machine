23c7f61c2f77ffcd7ffd168bc2dc27657b47a3a1
fvn70 <fvn_reg@mail.ru>
2022-02-04 16:04:37 +0700

    Coffee Machine. Stage 2. Ingredient calculator

---
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -0,0 +1,5 @@
+num = int(input("Write how many cups of coffee you will need:\n"))
+print(f"For {num} cups of coffee you will need:")
+print(f"{num * 200} ml of water")
+print(f"{num * 50} ml of milk")
+print(f"{num * 15} g of coffee beans")
