c437077a1482c99b708287fa65a293825add1134
fvn70 <fvn_reg@mail.ru>
2022-02-04 16:41:29 +0700

    Coffee Machine. Stage 3. Estimate the number of servings

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,5 +1,16 @@
+from math import floor
+
+cup = {'water': 200, 'milk': 50, 'beans': 15}
+water = int(input("Write how many ml of water the coffee machine has:\n"))
+milk = int(input("Write how many ml of milk the coffee machine has:\n"))
+beans = int(input("Write how many grams of coffee beans the coffee machine has:\n"))
 num = int(input("Write how many cups of coffee you will need:\n"))
-print(f"For {num} cups of coffee you will need:")
-print(f"{num * 200} ml of water")
-print(f"{num * 50} ml of milk")
-print(f"{num * 15} g of coffee beans")
+
+cups = floor(min(water / cup['water'], milk / cup['milk'], beans / cup['beans']))
+
+if cups < num:
+    print(f"No, I can make only {cups} cups of coffee")
+elif cups > num:
+    print(f"Yes, I can make that amount of coffee (and even {cups - num} more than that)")
+else:
+    print("Yes, I can make that amount of coffee")
