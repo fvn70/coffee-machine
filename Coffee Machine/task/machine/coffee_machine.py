1a211460447edee6acbb0cf77e002a462eb44348
fvn70 <fvn_reg@mail.ru>
2022-02-05 10:36:51 +0700

    Coffee Machine. Stage 5. Keep track of the supplies

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -3,24 +3,34 @@ kinds = {1: {'water': 250, 'milk': 0, 'beans': 16, 'cost': 4},
          2: {'water': 350, 'milk': 75, 'beans': 20, 'cost': 7},
          3: {'water': 200, 'milk': 100, 'beans': 12, 'cost': 6}}
 
-
 def state():
     print("\nThe coffee machine has:")
     print(f"{cm['water']} of water")
     print(f"{cm['milk']} of milk")
     print(f"{cm['beans']} of coffee beans")
     print(f"{cm['cups']} of disposable cups")
-    print(f"{cm['sum']} of money")
+    print(f"${cm['sum']} of money")
 
 
-def buy():
-    print("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    kind = int(input())
-    cm['water'] -= kinds[kind]['water']
-    cm['milk'] -= kinds[kind]['milk']
-    cm['beans'] -= kinds[kind]['beans']
-    cm['sum'] += kinds[kind]['cost']
-    cm['cups'] -= 1
+def buy(kind):
+    water = kinds[kind]['water']
+    milk = kinds[kind]['milk']
+    beans = kinds[kind]['beans']
+    if cm['water'] < water:
+        print("Sorry, not enough water!")
+    elif cm['milk'] < milk:
+        print("Sorry, not enough milk!")
+    elif cm['beans'] < beans:
+        print("Sorry, not enough beans!")
+    elif cm['cups'] < 1:
+        print("Sorry, not enough cups!")
+    else:
+        print("I have enough resources, making you a coffee!")
+        cm['water'] -= kinds[kind]['water']
+        cm['milk'] -= kinds[kind]['milk']
+        cm['beans'] -= kinds[kind]['beans']
+        cm['sum'] += kinds[kind]['cost']
+        cm['cups'] -= 1
 
 
 def fill():
@@ -35,12 +45,27 @@ def take():
     cm['sum'] = 0
 
 
-state()
-cmd = input("\nWrite action (buy, fill, take):\n")
-if cmd == 'buy':
-    buy()
-elif cmd == 'fill':
-    fill()
-elif cmd == 'take':
-    take()
-state()
+def read_cmd(cmd):
+    if cmd == 'fill':
+        fill()
+    elif cmd == 'take':
+        take()
+    elif cmd == 'remaining':
+        state()
+    else:
+        return
+
+
+while True:
+    print("\nWrite action (buy, fill, take, remaining, exit): ")
+    cmd = input()
+    if cmd == 'exit':
+        break
+    if cmd == 'buy':
+        cmd_2 = input("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: \n")
+        if cmd_2 == 'back':
+            continue
+        else:
+            buy(int(cmd_2))
+    else:
+        read_cmd(cmd)
